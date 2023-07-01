ba8f5d264b9b0f243a43a901e08290ce26ce832e
turovskiy <47497087+turovskiy@users.noreply.github.com>
2023-07-01 03:18:26 +0300

    Initial commit

---
+++ src-next/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import AppLayout from 'components/AppLayout';
+import Home from 'containers/Home';
+
+export default function Index() {
+  return (
+    <AppLayout>
+      <Home />
+    </AppLayout>
+  )
+}
